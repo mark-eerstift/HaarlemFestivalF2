a1a3d05f0391c01eb4c7e1cdd6eca9557287905f
Lars <lars.d.j@hotmail.com>
2018-12-28 15:53:13 +0100

    migration fixes

---
+++ ProjectWebApplicatie/Models/Jazz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectWebApplicatie.Models
+{
+    public class Jazz : Evenement
+    {
+        public virtual string Stage { get; set; }
+        public virtual Artiest Artiest { get; set; }
+    }
+}
