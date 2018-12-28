539c4fb9e1edb4cb21e57281497c37fe77930f6a
Jordy <Jordy@LAPTOP-V3IT5LEM>
2018-12-28 11:55:23 +0100

    Model/Database update v2 - 28-12-2018

--- ProjectWebApplicatie/Models/EventPage.cs
+++ ProjectWebApplicatie/Models/EventPage.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectWebApplicatie.Models
 {
     public class EventPage
     {
+        [Key]
+        public virtual int EventPageID { get; set; }
         public virtual List<string> Teksten { get; set; }
         public virtual List<Image> Afbeeldingen { get; set; }
 
--- ProjectWebApplicatie/Models/Image.cs
+++ ProjectWebApplicatie/Models/Image.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectWebApplicatie.Models
 {
     public class Image
     {
+        [Key]
+        public virtual int ImageID { get; set; }
         public virtual string Name { get; set; }
     }
 }
