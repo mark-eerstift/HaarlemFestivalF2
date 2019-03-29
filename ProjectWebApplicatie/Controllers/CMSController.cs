280ec7f5ee8708d763cd0268524260bbd3fa1e1b
Unknown <m4rkneut@gmail.com>
2019-03-29 13:07:15 +0100

    Alle event schedules inzien werkend

--- ProjectWebApplicatie/Controllers/CMSController.cs
+++ ProjectWebApplicatie/Controllers/CMSController.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectWebApplicatie.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -15,70 +16,128 @@ namespace ProjectWebApplicatie.Controllers
         private ProjectWebApplicatieContextDB db = new ProjectWebApplicatieContextDB();
         // GET: CMS
         [Authorize]
+        // GET: CMSTest
         public ActionResult Index()
         {
-            return View();
+            return View("~/Views/CMS/Dance/Index.cshtml", db.Evenements.ToList());
         }
 
-        [Authorize]
-        public ActionResult Jazz()
+        // GET: CMSTest/Details/5
+        public ActionResult Details(int? id)
         {
-            return View("~/Views/CMS/Jazz/Jazz.cshtml");
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evenement evenement = db.Evenements.Find(id);
+            if (evenement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evenement);
         }
 
-        [Authorize]
-        public ActionResult EditJazzPage()
+        // GET: CMSTest/Create
+        public ActionResult Create()
         {
-            return View("~/Views/CMS/Jazz/EditJazzPage.cshtml");
+            return View("~/Views/CMS/Dance/Create.cshtml");
         }
 
-        [Authorize]
-        public ActionResult EditJazzSchedule()
+        // POST: CMSTest/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EvenementID,Locatie,BeginTijd,EindTijd,TicketsTotaal,TicketsVerkocht,EvenementPrijs")] Evenement evenement, Dance dance, Jazz jazz, History history, Food food)
         {
-            return View("~/Views/CMS/Jazz/EditJazzSchedule.cshtml");
+            if (ModelState.IsValid)
+            {
+                dance.EvenementID = evenement.EvenementID;
+                jazz.EvenementID = evenement.EvenementID;
+                history.EvenementID = evenement.EvenementID;
+                food.EvenementID = evenement.EvenementID;
+
+                db.Evenements.Add(dance);
+                db.Evenements.Add(jazz);
+                db.Evenements.Add(history);
+                db.Evenements.Add(food);
+                db.SaveChanges();
+                
+            }
+
+            return View("~/Views/CMS/Dance/ViewDanceSales.cshtml", db.Evenements.ToList());
         }
 
-        [Authorize]
-        public ActionResult ViewJazzSales(int id)
+        // GET: CMSTest/Edit/5
+        public ActionResult Edit(int? id)
         {
-            return View("~/Views/CMS/Jazz/ViewJazzSales.cshtml");
-
-
-
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evenement evenement = db.Evenements.Find(id);
+            if (evenement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evenement);
         }
 
-
-
-        [Authorize]
-        public ActionResult Food()
+        // POST: CMSTest/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EvenementID,Locatie,BeginTijd,EindTijd,TicketsTotaal,TicketsVerkocht,EvenementPrijs")] Evenement evenement)
         {
-            return View("~/Views/CMS/Food/Food.cshtml");
+            if (ModelState.IsValid)
+            {
+                db.Entry(evenement).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(evenement);
         }
 
-        [Authorize]
-        public ActionResult ViewFoodSales()
+        // GET: CMSTest/Delete/5
+        public ActionResult Delete(int? id)
         {
-            return View("~/Views/CMS/Food/ViewFoodSales.cshtml");
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evenement evenement = db.Evenements.Find(id);
+            if (evenement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evenement);
         }
 
-        [Authorize]
-        public ActionResult EditFoodRestaurants()
+        // POST: CMSTest/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
         {
-            return View("~/Views/CMS/Food/EditFoodRestaurants.cshtml");
+            Evenement evenement = db.Evenements.Find(id);
+            db.Evenements.Remove(evenement);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        [Authorize]
-        public ActionResult EditFoodPage()
+        protected override void Dispose(bool disposing)
         {
-            return View("~/Views/CMS/Food/EditFoodPage.cshtml");
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
-        [Authorize]
-        public ActionResult Dance()
-        {
-            return View("~/Views/CMS/Dance/Dance.cshtml");
-        }
-
+       /* Ik heb hier overwogen om hier twee methodes van te maken.
+        * Maar ik weet niet zo goed hoe ik het beste deze methode door kan geven welk type event hij op moet halen. 
+        * Mijn pogingen om dit voor elkaar te krijgen werden al snel erg lelijk. 
+        * */
         [Authorize]
         public ActionResult ViewDanceSales()
         {
@@ -87,6 +146,11 @@ namespace ProjectWebApplicatie.Controllers
             
             return View(("~/Views/CMS/Dance/ViewDanceSales.cshtml"), dances);
         }
+
+
+
+
+
 
         [Authorize]
         public ActionResult ViewDanceSalesByDate(DateTime date)
@@ -98,41 +162,76 @@ namespace ProjectWebApplicatie.Controllers
             return View(("~/Views/CMS/Dance/ViewDanceSales.cshtml"), dances);
         }
 
+
+        //Maakt in de desbetreffende view een Null object aan van de benodigde event. Checkt of de event null is en zo ja, laadt de pagina.
         [Authorize]
-        public ActionResult EditDanceSchedule()
+        public ActionResult ViewEventSales(Dance dance, Jazz jazz, History history, Food food)
         {
-            return View("~/Views/CMS/Dance/EditDanceSchedule.cshtml");
+            if (dance == null)
+            {
+                List<Dance> dances = db.Dances.ToList();
+                return View(("~/Views/CMS/Dance/ViewDanceSales.cshtml"), dances);
+            }
+            else if (jazz == null)
+            {
+                List<Jazz> jazzs = db.Jazzs.ToList();
+                return View(("~/Views/CMS/Dance/ViewJazzSales.cshtml"), jazzs);
+            }
+            else if (history == null)
+            {
+                List<History> histories = db.Historys.ToList();
+                return View(("~/Views/CMS/Dance/ViewHistorySales.cshtml"), histories);
+            }
+            else if (food == null)
+            {
+                List<Food> foods = db.Foods.ToList();
+                return View(("~/Views/CMS/Dance/ViewFoodSales.cshtml"), foods);
+            }
+
+
+            return View("~/Views/CMS/Index.cshtml");
         }
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         [Authorize]
-        public ActionResult EditDancePage()
+        public ActionResult ViewJazzSales()
         {
-            return View("~/Views/CMS/Dance/EditDancePage.cshtml");
+            List<Jazz> jazz = new List<Jazz>();
+            jazz = db.Jazzs.ToList();
+
+            return View(("~/Views/CMS/Dance/ViewDanceSales.cshtml"), jazz);
         }
 
-        [Authorize]
-        public ActionResult Historic()
-        {
-            return View("~/Views/CMS/Historic/Historic.cshtml");
-        }
 
         [Authorize]
-        public ActionResult ViewHistoricSales()
+        public ActionResult ViewJazzSalesByDate(DateTime date)
         {
-            return View("~/Views/CMS/Historic/ViewHistoricSales.cshtml");
+            List<Jazz> jazz = new List<Jazz>();
+            jazz = db.Jazzs.ToList();
+            jazz.RemoveAll(Jazz => Jazz.BeginTijd.Date != date.Date);
+
+            return View(("~/Views/CMS/Dance/ViewDanceSales.cshtml"), jazz);
         }
 
-        [Authorize]
-        public ActionResult EditHistoricSchedule()
-        {
-            return View("~/Views/CMS/Historic/EditHistoricSchedule.cshtml");
-        }
 
-        [Authorize]
-        public ActionResult EditHistoricPage()
-        {
-            return View("~/Views/CMS/Historic/EditHistoricPage.cshtml");
-        }
 
 
 
