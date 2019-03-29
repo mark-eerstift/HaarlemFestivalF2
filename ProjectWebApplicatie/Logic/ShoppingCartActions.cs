2a4c18487f8d911bfe990091f576fc86c2017b4c
Unknown <lars.d.j@hotmail.com>
2019-03-30 00:08:56 +0100

    ShoppingCartActions class added for logic

---
+++ ProjectWebApplicatie/Logic/ShoppingCartActions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectWebApplicatie.Models;
+
+namespace ProjectWebApplicatie.Logic
+{
+    public class ShoppingCartActions
+    {
+        public string ShoppingCartId { get; set; }
+
+        private ProjectWebApplicatieContextDB db = new ProjectWebApplicatieContextDB();
+
+        public const string CartSessionId = "CardId";
+    }
+}
